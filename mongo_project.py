78958459feab00685326a1ff4f16ab626d87badf
Sean Young <seanyoung247@gmail.com>
2020-11-09 23:41:57 -0500

    MongoDB python lesson

---
+++ mongo_project.py
@@ -0,0 +1,152 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myFirstDB"
+COLLECTION = "celebrities"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
+def show_menu():
+    print("")
+    print("1. Add a record")
+    print("2. Find a record by name")
+    print("3. Edit a record")
+    print("4. Delete a record")
+    print("5. Exit")
+
+    option = input ("Enter option: ")
+    return option
+
+
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({"first": first.lower(), "last": last.lower()})
+    except:
+        print("Error accessing the database")
+
+    if not doc:
+        print("")
+        print("Error! No results found.")
+
+    return doc
+
+
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_color = input("Enter hair color > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+
+    new_doc = {
+        "first": first.lower(),
+        "last": last.lower(),
+        "dob": dob,
+        "gender": gender.lower(),
+        "hair_color": hair_color.lower(),
+        "occupation": occupation.lower(),
+        "nationality": nationality.lower()
+    }
+
+    try:
+        coll.insert_one(new_doc)
+        print("")
+        print("Record inserted")
+    except:
+        print("error adding record")
+
+
+def print_record(doc):
+    if doc:
+        print("")
+        for k,v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+
+def find_record():
+    doc = get_record()
+    print_record(doc)
+
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        update_doc = {}
+        print("")
+        for k,v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "] > ").lower()
+
+                if update_doc[k] == "":
+                    update_doc[k] = v
+
+        try:
+            coll.update_one(doc, {"$set": update_doc})
+            print("")
+            print("Record updated")
+        except:
+            print("Error! Update failed!")
+
+
+def delete_record():
+    doc = get_record()
+    print_record(doc)
+
+    print("")
+    conf = input("Is this the record you want to delete?\nY or N > ").lower()
+    print("")
+
+    if conf == 'y':
+        try:
+            coll.remove(doc)
+            print("Record deleted!")
+        except:
+            print("Error! Unable to delete record")
+    else:
+        print("Record not deleted")
+
+
+def main_loop():
+    while True:
+        option = show_menu()
+        if option == "1":       #Add a record
+            add_record()
+        elif option == "2":     #Find a record
+            find_record()
+        elif option == "3":     #Edit a record
+            edit_record()
+        elif option == "4":     #Delete a record
+            delete_record()
+        elif option == "5":     #Exit
+            conn.close()
+            break;
+        else:
+            print("Invalid option")
+
+        print("")
+
+
+conn = mongo_connect(MONGO_URI)
+
+coll = conn[DATABASE][COLLECTION]
+
+main_loop()
